aa433f3a1b5d71daeb85bc20e4c6a9f80b07b2c7
Charles Lockner <charleslockner@gmail.com>
2014-10-15 01:56:37 -0700

    now loading from json files

--- js/src/models.js
+++ js/src/models.js
@@ -1,126 +1,36 @@
-var ModelLibrary = function(gl) {
-	var models = {
+Portal.prototype.loadModels = function() {
+	this.models = {
 		cube : null
 	};
 
-	models.cube = this.initCube(gl);
-
-
-	// $.getJSON("assets/cube.json", function(json) {
-	//     console.log(json);
-	// });
-
-
-	return models;
+	this.initCube();
 }
 
-ModelLibrary.prototype.cubeModel = {
-	vertices : [
-		// Front face
-		-1.0, -1.0,  1.0,
-		1.0, -1.0,  1.0,
-		1.0,  1.0,  1.0,
-		-1.0,  1.0,  1.0,
+Portal.prototype.initCube = function() {
+	var self = this;
 
-		// Back face
-		-1.0, -1.0, -1.0,
-		-1.0,  1.0, -1.0,
-		1.0,  1.0, -1.0,
-		1.0, -1.0, -1.0,
+	$.getJSON("assets/pillar.json", function(json) {
+      console.log(json);
 
-		// Top face
-		-1.0,  1.0, -1.0,
-		-1.0,  1.0,  1.0,
-		1.0,  1.0,  1.0,
-		1.0,  1.0, -1.0,
+      var cube = {
+	  	   faces : json.metadata.faces,
+	   	vbo : self.gl.createBuffer(),
+	   	nbo : self.gl.createBuffer(),
+	   	ibo : self.gl.createBuffer()
+	   }
 
-		// Bottom face
-		-1.0, -1.0, -1.0,
-		1.0, -1.0, -1.0,
-		1.0, -1.0,  1.0,
-		-1.0, -1.0,  1.0,
+	   // vertex positions
+	   self.gl.bindBuffer(self.gl.ARRAY_BUFFER, cube.vbo);
+	   self.gl.bufferData(self.gl.ARRAY_BUFFER, new Float32Array(json.vertices), self.gl.STATIC_DRAW);
 
-		// Right face
-		1.0, -1.0, -1.0,
-		1.0,  1.0, -1.0,
-		1.0,  1.0,  1.0,
-		1.0, -1.0,  1.0,
+	   // vertex normals
+	   self.gl.bindBuffer(self.gl.ARRAY_BUFFER, cube.nbo);
+	   self.gl.bufferData(self.gl.ARRAY_BUFFER, new Float32Array(json.normals), self.gl.STATIC_DRAW);
 
-		// Left face
-		-1.0, -1.0, -1.0,
-		-1.0, -1.0,  1.0,
-		-1.0,  1.0,  1.0,
-		-1.0,  1.0, -1.0 ],
-	
-	normals : [
-		// Front face
-		0.0, 0.0, 1.0,
-		0.0, 0.0, 1.0,
-		0.0, 0.0, 1.0,
-		0.0, 0.0, 1.0,
+	   // indices to vertices
+	   self.gl.bindBuffer(self.gl.ELEMENT_ARRAY_BUFFER, cube.ibo);
+	   self.gl.bufferData(self.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(json.faces), self.gl.STATIC_DRAW);
 
-		// Back face
-		0.0, 0.0, -1.0,
-		0.0, 0.0, -1.0,
-		0.0, 0.0, -1.0,
-		0.0, 0.0, -1.0,
-
-		// Top face
-		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
-
-		// Bottom face
-		0.0, -1.0, 0.0,
-		0.0, -1.0, 0.0,
-		0.0, -1.0, 0.0,
-		0.0, -1.0, 0.0,
-
-		// Right face
-		1.0, 0.0, 0.0,
-		1.0, 0.0, 0.0,
-		1.0, 0.0, 0.0,
-		1.0, 0.0, 0.0,
-
-		// Left face
-		-1.0, 0.0, 0.0,
-		-1.0, 0.0, 0.0,
-		-1.0, 0.0, 0.0,
-		-1.0, 0.0, 0.0 ],
-
-	indices : [
-		0, 1, 2,      0, 2, 3,      // Front face
-		4, 5, 6,      4, 6, 7,      // Back face
-		8, 9, 10,     8, 10, 11,    // Top face
-		12, 13, 14,   12, 14, 15,   // Bottom face
-		16, 17, 18,   16, 18, 19,   // Right face
-		20, 21, 22,   20, 22, 23 ], // Left face
-}
-
-ModelLibrary.prototype.initCube = function(gl) {
-   // vertex positions
-   var cube = {
-   	  metadata : { faces : 12 },
-   	  vbo : null,
-   	  nbo : null,
-   	  ibo : null
-   };
-
-   // vertex positions
-   cube.vbo = gl.createBuffer();
-   gl.bindBuffer(gl.ARRAY_BUFFER, cube.vbo);
-   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.cubeModel.vertices), gl.STATIC_DRAW);
-
-   // vertex normals
-   cube.nbo = gl.createBuffer();
-   gl.bindBuffer(gl.ARRAY_BUFFER, cube.nbo);
-   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.cubeModel.normals), gl.STATIC_DRAW);
-
-   // indices to vertices
-   cube.ibo = gl.createBuffer();
-   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube.ibo);
-   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.cubeModel.indices), gl.STATIC_DRAW);
-
-   return cube;
+	   self.models.cube = cube;
+	});
 }
--- js/src/portal.js
+++ js/src/portal.js
@@ -37,7 +37,7 @@ Portal.prototype.initShaders = function() {
 }
 
 Portal.prototype.initModels = function() {
-   this.models = new ModelLibrary(this.gl);
+   this.loadModels();
 }
 
 Portal.prototype.initSystem = function() {
@@ -101,16 +101,18 @@ Portal.prototype.sendEntityIndependantShaderData = function() {
 Portal.prototype.drawEntity = function(entity) {
    var model = this.models[entity.model];
 
-   var modelM = this.makeModelMatrix(entity);
-   this.gl.uniformMatrix4fv(this.shaderProgram.uModelMatrix, false, modelM);
+   if (model) {
+      var modelM = this.makeModelMatrix(entity);
+      this.gl.uniformMatrix4fv(this.shaderProgram.uModelMatrix, false, modelM);
 
-   this.gl.bindBuffer(this.gl.ARRAY_BUFFER, model.vbo);
-   this.gl.vertexAttribPointer(this.shaderProgram.aVertexPosition, 3, this.gl.FLOAT, false, 0, 0);
-   this.gl.bindBuffer(this.gl.ARRAY_BUFFER, model.nbo);
-   this.gl.vertexAttribPointer(this.shaderProgram.aVertexNormal, 3, this.gl.FLOAT, false, 0, 0);
-   this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, model.ibo);
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, model.vbo);
+      this.gl.vertexAttribPointer(this.shaderProgram.aVertexPosition, 3, this.gl.FLOAT, false, 0, 0);
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, model.nbo);
+      this.gl.vertexAttribPointer(this.shaderProgram.aVertexNormal, 3, this.gl.FLOAT, false, 0, 0);
+      this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, model.ibo);
 
-   this.gl.drawElements(this.gl.TRIANGLES, 3 * model.metadata.faces, this.gl.UNSIGNED_SHORT, 0);
+      this.gl.drawElements(this.gl.TRIANGLES, 3 * model.faces, this.gl.UNSIGNED_SHORT, 0);
+   }
 }
 
 Portal.prototype.makeModelMatrix = function(entity) {
