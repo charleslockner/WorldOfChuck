eb83cafaf4ae6a7438f893acda35e0ac44812e4a
Charles Lockner <charleslockner@gmail.com>
2014-10-24 15:56:44 -0700

    ingore is working?

--- config.js
+++
@@ -1,9 +0,0 @@
-var getAddress = function() {
-   return "0.0.0.0";
-};
-var getPort = function() { 
-   return 8080;
-};
-
-module.exports.getAddress = getAddress;
-module.exports.getPort = getPort;
