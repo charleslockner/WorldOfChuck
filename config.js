3a4236d45ec36a2f873e3c519ea0f23aab0f65fd
Ubuntu <ubuntu@ip-172-31-7-239.us-west-1.compute.internal>
2014-10-25 18:53:27 +0000

    fixed git ingnore

--- config.js
+++
@@ -1,9 +0,0 @@
-var getAddress = function() {
-   return "127.0.0.1";
-};
-var getPort = function() { 
-   return 8080;
-};
-
-module.exports.getAddress = getAddress;
-module.exports.getPort = getPort;
